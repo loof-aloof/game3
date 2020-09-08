47c66239476b1d7b8fcf3ae3acccfb306b3f046b
loof-aloof <69801386+loof-aloof@users.noreply.github.com>
2020-09-08 08:35:06 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let Sprite1 = sprites.create(img`
+    d 1 d d d d d d d d d d d d d d
+    d d . . d d d d d d d d d d d d
+    d . . . . d b d d d d d 1 1 d d
+    d . . . . . . d d d d d 1 1 d d
+    d d d . . . . . d d d d d d d d
+    d d d . . . . . . . d d d d d d
+    d 1 d d . . . . . . . d d d d d
+    d d d d . . . . . . . . . d b d
+    d d d d . . . . . . . . . . . d
+    d d d d . . . . . . . . . . . .
+    d d d b d . . . . . . . . . . .
+    d d d d d d . . . . . . . . . .
+    d 6 d d d d d . . . . . . . d d
+    6 7 6 d 6 7 d d d 7 6 7 7 6 d d
+    7 7 6 7 7 6 d 6 6 7 6 7 6 7 6 6
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+`)
+let statusbar = statusbars.create(140, 4, 0)
